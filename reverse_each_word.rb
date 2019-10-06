6fecf09937155a449ba3871eaaefcd0bd9dbcad8
Parker Bacall <pbacall2@gmail.com>
2019-10-06 02:00:10 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(string)
+reversed_array = []
+array = string.split()
+
+array.collect do |string_array|
+  reversed_array.push("#{string_array.reverse}")
+end
+ return reversed_array.join(" ")
+end
